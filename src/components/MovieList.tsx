95cf3fe8558c50a0bf39c85e7d3cb4ed78c5e16f
Theodore Halpern <theohal@meta.com>
2024-06-11 17:16:38 -0700

    added loading indicater

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -12,8 +12,10 @@ interface Props {
 
 const MovieList = ({ searchQuery, currentSort, movieDBPageNumber, setMovieDBPageNumber }: Props) => {
     const [moviesJSON, setMoviesJSON] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const loadMovies = async () => {
+        setLoading(true)
 
         let url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US`;
         if (searchQuery !== '') {
@@ -30,6 +32,11 @@ const MovieList = ({ searchQuery, currentSort, movieDBPageNumber, setMovieDBPage
                 accept: 'application/json',
             }
         };
+        // Clears the page if the query has changed (instead of new page)
+        if(movieDBPageNumber == 1) {
+            setMoviesJSON([])
+        }
+
         const response = await fetch(url, options);
         const data = await response.json();
         console.log(data);
@@ -48,11 +55,13 @@ const MovieList = ({ searchQuery, currentSort, movieDBPageNumber, setMovieDBPage
         <div className='bodyContainer'>
             <h2>Movie List</h2>
             <div className="movieList">
+
                 {moviesJSON.map(function (movie, i) {
                     return (
                         <MovieCard movie={movie} key={i} />
                     )
                 })}
+                {loading ? <div className="loading">Loading...</div> : null}
             </div>
             <button className="loadMore" onClick={() => { setMovieDBPageNumber(movieDBPageNumber + 1) }}>Load More</button>
         </div>
