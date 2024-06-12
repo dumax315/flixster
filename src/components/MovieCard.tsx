26ce45a924a957eb37ef65f6b9ac701d32217a37
Theodore Halpern <theohal@meta.com>
2024-06-11 17:26:31 -0700

    feat added hover to playlists and moved rating

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -21,7 +21,7 @@ const MovieCard = (props: Props) => {
             <div className="MovieCard" onClick={openMovieDetails}>
                 <img draggable="false" className='MoviePoster' src={"https://image.tmdb.org/t/p/w500/" + props.movie.poster_path} />
                 <p className='MovieTitle'>{props.movie.title}</p>
-                <p className='Movie Rating'>{props.movie.vote_average}</p>
+                <p className='MovieRating'>{props.movie.vote_average.toFixed(1)}</p>
             </div>
             <MovieDetails movie={props.movie} isOpen={isMovieDetailsOpen} closeModalFunction={closeMovieDetails} />
         </>
