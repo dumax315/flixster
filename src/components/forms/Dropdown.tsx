9cd5fbbe7872598a922c06b6df526997dfe629a3
Theodore Halpern <theohal@meta.com>
2024-06-12 14:48:24 -0700

    fix: fixed load more button and repositioned it

--- src/components/forms/Dropdown.tsx
+++ src/components/forms/Dropdown.tsx
@@ -16,7 +16,7 @@ const Dropdown = (props: Props) => {
                 <option value="original_title.desc">original_title.desc</option>
                 {/* <option value="popularity.asc">popularity.asc</option> */}
                 <option value="popularity.desc">popularity.desc</option>
-                <option value="revenue.asc">revenue.asc</option>
+                {/* <option value="revenue.asc">revenue.asc</option> */}
                 <option value="revenue.desc">revenue.desc</option>
                 <option value="primary_release_date.asc">primary_release_date.asc</option>
                 <option value="title.asc">title.asc</option>
@@ -24,7 +24,7 @@ const Dropdown = (props: Props) => {
                 <option value="primary_release_date.desc">primary_release_date.desc</option>
                 {/* <option value="vote_average.asc">vote_average.asc</option> */}
                 <option value="vote_average.desc">vote_average.desc</option>
-                <option value="vote_count.asc">vote_count.asc</option>
+                {/* <option value="vote_count.asc">vote_count.asc</option> */}
                 <option value="vote_count.desc">vote_count.desc</option>
             </select>
         </>
