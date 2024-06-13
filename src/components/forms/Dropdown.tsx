4eff0ebd39afe5d57f6d733f40667ab9352de376
Theodore Halpern <theohal@meta.com>
2024-06-13 14:15:42 -0700

    style: Cleaned the dropdown items

--- src/components/forms/Dropdown.tsx
+++ src/components/forms/Dropdown.tsx
@@ -13,20 +13,14 @@ const Dropdown = ({ currentValue, handleValueChange, selectionName }: Props) => 
         <div>
             <label className="dropdownLabel" htmlFor={selectionName}>{selectionName}</label>
                 <select id={selectionName} className="dropdown" value={currentValue} onChange={handleValueChange}>
-                    {/* <option value="original_title.asc">original_title.asc</option>
-                    <option value="original_title.desc">original_title.desc</option> */}
-                    {/* <option value="popularity.asc">popularity.asc</option> */}
-                    <option value="popularity.desc">popularity.desc</option>
-                    {/* <option value="revenue.asc">revenue.asc</option> */}
-                    <option value="revenue.desc">revenue.desc</option>
-                    <option value="primary_release_date.asc">primary_release_date.asc</option>
-                    <option value="title.asc">title.asc</option>
-                    <option value="title.desc">title.desc</option>
-                    <option value="primary_release_date.desc">primary_release_date.desc</option>
-                    {/* <option value="vote_average.asc">vote_average.asc</option> */}
-                    <option value="vote_average.desc">vote_average.desc</option>
-                    {/* <option value="vote_count.asc">vote_count.asc</option> */}
-                    <option value="vote_count.desc">vote_count.desc</option>
+                    <option value="popularity.desc">Popularity</option>
+                    <option value="revenue.desc">Revenue</option>
+                    <option value="title.asc">Title A-Z</option>
+                    <option value="title.desc">Title Z-A</option>
+                    <option value="primary_release_date.asc">Oldest-Newest</option>
+                    <option value="primary_release_date.desc">Newest-Oldest</option>
+                    <option value="vote_average.desc">Highest Rated</option>
+                    <option value="vote_count.desc">Most Votes</option>
                 </select>
         </div>
 
